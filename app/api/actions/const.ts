48c525a7f5abc21de22408361c2feddf8407a372
Sachiik0 <mmasanjuan@tip.edu.ph>
2024-07-31 09:34:15 +0800

    trying transfer-sol program

---
+++ app/api/actions/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "9HKEqxYorpmmeQnyCN8T3HpYmV8f2dkpfWA7YsxuK5EL", // devnet wallet
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
--- app/api/actions/route.ts
+++ app/api/actions/route.ts
@@ -1,64 +1,150 @@
-import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS } from '@solana/actions';
-import { PublicKey, SystemProgram, Transaction, Connection, clusterApiUrl } from '@solana/web3.js';
+import {ActionPostResponse, ACTIONS_CORS_HEADERS, createPostResponse, ActionGetResponse, ActionPostRequest} from "@solana/actions";
+import {clusterApiUrl,Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
+import { DEFAULT_SOL_ADDRESS, DEFAULT_SOL_AMOUNT } from "./const";
 
-export async function GET(request: Request) {
-    try {
+
+export const GET  = async (req: Request) => {
+    try{    
+        const requestUrl = new URL(req.url);
+        const { toPubkey } = validatedQueryParams(requestUrl);
+
+        const baseHref = new URL(
+            `/api/actions/?to=${toPubkey.toBase58()}`,
+            requestUrl.origin
+        ).toString();
+
         const payload: ActionGetResponse = {
-            icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLT7i1KaPG3JWvgdPnWK9h3h3IxUxumVQsEA&s",
-            description: "Hi, I'm Marc Anthony M. San Juan, a dedicated student from the Philippines. I am passionate about blockchain technology and its potential to revolutionize various industries. I am currently seeking funds to attend Solana Breakpoint 2024, a premier event that will allow me to gain invaluable insights, network with industry leaders, and further my education and involvement in the blockchain space. In the Philippines, ''limos'' is a term that means alms, symbolizing a plea for help. As a determined student, I am reaching out for your support to make this opportunity a reality. Attending Solana Breakpoint 2024 is a pivotal step in my journey, and with your help, I can achieve my goals and contribute significantly to the blockchain community. Your generous support will not only help me attend this event but also empower me to bring back knowledge and innovation to the Philippines, fostering growth and development in our local blockchain ecosystem.",
-            title: "Blink Limos for Solana Breakpoint 2024",
-            label: 'Donate'
-        };
-        return new Response(JSON.stringify(payload), { headers: ACTIONS_CORS_HEADERS });
+            title: '',
+            icon: '',
+            description:'',
+            label:'',
+            links:{
+                actions:[
+                    {
+                        label: "Send SOL", // button text
+                        href: `${baseHref}&amount={amount}`, // this href will have a text input
+                        parameters: [
+                            {
+                                name: "amount", // parameter name in the `href` above
+                                label: "Enter the amount of SOL to send", // placeholder of the text input
+                                required: true
+                            }
+                        ]
+                    }
+                ]
+            }
+            
+        }
+        return Response.json(payload, {
+            headers: ACTIONS_CORS_HEADERS,
+        });
     } catch (err) {
-        console.error(err);
-        const message = typeof err === "string" ? err : "An unknown error occurred";
+        console.log(err);
+        let message = "An unknown error occurred";
+        if (typeof err == "string") message = err;
         return new Response(message, {
             status: 400,
             headers: ACTIONS_CORS_HEADERS,
         });
     }
+}
+
+
+  // DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
+  // THIS WILL ENSURE CORS WORKS FOR BLINKS
+export const OPTIONS = async (req: Request) => {
+    return new Response(null, { headers: ACTIONS_CORS_HEADERS });
 };
 
-export async function POST(request: Request) {
+export const POST = async (req:Request) => {
+    try{
+        const requestUrl = new URL(req.url);
+    const { amount, toPubkey } = validatedQueryParams(requestUrl);
+
+    const body: ActionPostRequest = await req.json();
+
+    // validate the client provided input
+    let account: PublicKey;
     try {
-        const connection = new Connection(clusterApiUrl('mainnet-beta'));
-        const requestBody: ActionPostRequest = await request.json();
-        const userPubkey = new PublicKey(requestBody.account);
-        console.log(userPubkey.toString());
-
-        const tx = new Transaction();
-        const ix = SystemProgram.transfer({
-            fromPubkey: userPubkey,
-            toPubkey: new PublicKey("9HKEqxYorpmmeQnyCN8T3HpYmV8f2dkpfWA7YsxuK5EL"),
-            lamports: 1000000
-        });
-        tx.add(ix);
-
-        const { blockhash } = await connection.getLatestBlockhash({ commitment: "finalized" });
-        console.log("using blockhash " + blockhash);
-
-        tx.feePayer = userPubkey;
-        tx.recentBlockhash = blockhash;
-
-        const serialTX = tx.serialize({ requireAllSignatures: false, verifySignatures: false }).toString("base64");
-
-        const response: ActionPostResponse = {
-            transaction: serialTX,
-            message: `hello ${userPubkey.toString()}. This will help Me-Aw so much. Thank you very much.🤙🏿🤙🏿🤙🏿`,
-        };
-
-        return new Response(JSON.stringify(response), { headers: ACTIONS_CORS_HEADERS });
+        account = new PublicKey(body.account);
     } catch (err) {
-        console.error(err);
-        const message = typeof err === "string" ? err : "An unknown error occurred";
+        return new Response('Invalid "account" provided', {
+            status: 400,
+            headers: ACTIONS_CORS_HEADERS,
+        });
+    }
+
+    const connection = new Connection(
+        process.env.SOLANA_RPC! || clusterApiUrl("devnet"),
+    );
+
+    const minimumBalance = await connection.getMinimumBalanceForRentExemption(
+        0, // note: simple accounts that just store native SOL have `0` bytes of data
+    );
+
+    if (amount * LAMPORTS_PER_SOL < minimumBalance) {
+        throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
+    }
+
+    const transferSolInstruction = SystemProgram.transfer({
+        fromPubkey: account,
+        toPubkey: toPubkey,
+        lamports: amount * LAMPORTS_PER_SOL,
+    });
+
+    const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+
+    const transaction = new Transaction({
+        feePayer: account,
+        blockhash,
+        lastValidBlockHeight,
+    }).add(transferSolInstruction);
+
+    const payload: ActionPostResponse = await createPostResponse({
+        fields: {
+            transaction,
+            message: `Send ${amount} SOL to ${toPubkey.toBase58()}`,
+        },
+        // note: no additional signers are needed
+        // signers: [],
+    });
+
+    } catch (err) {
+        console.log(err);
+        let message = "An unknown error occurred";
+        if (typeof err == "string") message = err;
         return new Response(message, {
             status: 400,
             headers: ACTIONS_CORS_HEADERS,
         });
     }
-};
+}
 
-// export async function OPTION(request: Request){
-//     return new Response(null, {headers: ACTIONS_CORS_HEADERS,});
-// }
+
+
+function validatedQueryParams(requestUrl: URL) {
+    let toPubkey: PublicKey = DEFAULT_SOL_ADDRESS;
+    let amount: number = DEFAULT_SOL_AMOUNT;
+
+    try {
+        if (requestUrl.searchParams.get("to")) {
+            toPubkey = new PublicKey(requestUrl.searchParams.get("to")!);
+        }
+    } catch (err) {
+        throw "Invalid input query parameter: to";
+    }
+
+    try {
+        if (requestUrl.searchParams.get("amount")) {
+            amount = parseFloat(requestUrl.searchParams.get("amount")!);
+        }
+        if (amount <= 0) throw "amount is too small";
+    } catch (err) {
+        throw "Invalid input query parameter: amount";
+    }
+
+    return {
+        amount,
+        toPubkey,
+    };
+}
